a018818a58cf0b439c502b61667ce59a7f443af3
Zachary Wilson <zw96042@gmail.com>
2025-06-07 15:16:04 -0500

    Feat: Light mode message detailed view

--- app/inbox/[message].tsx
+++ app/inbox/[message].tsx
@@ -10,35 +10,35 @@ const MessageDetails = () => {
     <View className='bg-primary flex-1'>
         <ScrollView className='flex-1'>
             <View>
-                <Text className='text-slate-400 font-bold ml-4 mt-3 text-sm'>Subject</Text>
+                <Text className='text-accent font-bold ml-4 mt-3 text-sm'>Subject</Text>
                 <View className="bg-cardColor rounded-lg p-3 mx-4 mt-3">
-                    <Text className="text-slate-200 leading-5">
+                    <Text className="text-main leading-5">
                         {message}
                     </Text>
                 </View>
 
                 <View className='flex-row items-center'>
                     <View className='mt-4 px-4 w-[50%]'>
-                        <Text className='text-slate-400 font-bold text-sm mb-4'>From</Text>
+                        <Text className='text-accent font-bold text-sm mb-4'>From</Text>
                         <View className="bg-cardColor rounded-lg p-3 w-full">
-                            <Text className="text-slate-200 leading-5">
+                            <Text className="text-main leading-5">
                                 {from}
                             </Text>
                         </View>
                     </View>
                     <View className='mt-4 px-4 w-[50%]'>
-                        <Text className='text-slate-400 font-bold text-sm mb-4'>Date</Text>
+                        <Text className='text-accent font-bold text-sm mb-4'>Date</Text>
                         <View className="bg-cardColor rounded-lg p-3 w-full">
-                            <Text className="text-slate-200 leading-5">
+                            <Text className="text-main leading-5">
                                 {date.slice(date.indexOf(" ")+1)}
                             </Text>
                         </View>
                     </View>
                 
                 </View>
-                <Text className='text-slate-400 font-bold ml-4 mt-3 text-sm'>Message</Text>
-                <View className="bg-cardColor rounded-lg p-3 mx-4 mt-3">
-                    <Text className="text-slate-200 leading-5">
+                <Text className='text-accent font-bold ml-4 mt-3 text-sm'>Message</Text>
+                <View className="bg-cardColor rounded-lg p-3 mx-4 mt-3 mb-8">
+                    <Text className="text-main leading-5">
                         {content}
                     </Text>
                 </View>
