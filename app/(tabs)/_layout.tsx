56e2baa2188c0ba80e2d5c1f394ba2bbe27f82d6
Zachary Wilson <zw96042@gmail.com>
2025-06-10 21:47:11 -0500

    Fix: Bring modal all the way to root, use same modal across other screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,143 +1,413 @@
-import { View, Text, TouchableOpacity, useColorScheme } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { images } from '@/constants/images'
+import { Image } from 'react-native'
+import { icons } from '@/constants/icons'
 import { Ionicons } from '@expo/vector-icons'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
-import BottomSheet, { BottomSheetBackdrop, BottomSheetFlatList } from '@gorhom/bottom-sheet'
-import { useBottomSheet, BottomSheetProvider, TermLabel } from '@/context/BottomSheetContext'
-import { colors } from '@/utils/colorTheme'
-
-const terms: TermLabel[] = ['Q1 Grades', 'Q2 Grades', 'SM1 Grade', 'Q3 Grades', 'Q4 Grades', 'SM2 Grades'];
-
-
-
-const InnerLayout = () => {
-  const { selectedCategory, setSelectedCategory, bottomSheetRef } = useBottomSheet()
-  const colorScheme = useColorScheme()
-  const cardColor = colorScheme === 'dark' ? colors.cardColor.dark : colors.cardColor.light
-
-  return (
-    <>
-      <Tabs
-        screenOptions={{
-          tabBarShowLabel: false,
-          tabBarItemStyle: {
-            flex: 1,
-          },
-          tabBarStyle: {
-            borderTopWidth: 0,
-            height: 70,
-            paddingBottom: 8,
-            position: 'absolute',
-          },
-          tabBarBackground: () => (
-            <View className="flex-1 bg-nav border-t border-gray-200 dark:border-transparent" />
-          ),
-        }}
-      >
-        <Tabs.Screen 
-          name="index"
-          options={{
-            title: "Courses",
-            headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <View className="items-center justify-center w-[180%] mt-5 h-[80%]">
-                <Ionicons name="school-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
-                <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Courses</Text>
-              </View>
-            )
-          }}
-        />
-        <Tabs.Screen 
-          name="assignments"
-          options={{
-            title: "Assignments",
-            headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <View className="items-center justify-center w-[240%] mt-5 h-[80%]">
-                <Ionicons name="document-text-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
-                <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Assignments</Text>
-              </View>
-            )
-          }}
-        />
-        <Tabs.Screen 
-          name="inbox"
-          options={{
-            title: "Inbox",
-            headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <View className="items-center justify-center w-[180%] mt-5 h-[80%]">
-                <Ionicons name="file-tray-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
-                <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Inbox</Text>
-              </View>
-            )
-          }}
-        />
-        <Tabs.Screen 
-          name="profile"
-          options={{
-            title: "Settings",
-            headerShown: false,
-            tabBarIcon: ({ focused }) => (
-              <View className="items-center justify-center w-[180%] mt-5 h-[80%]">
-                <Ionicons name="settings-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
-                <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Settings</Text>
-              </View>
-            )
-          }}
-        />
-      </Tabs>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={-1}
-        snapPoints={['42%']}
-        enablePanDownToClose={true}
-        backgroundStyle={{ backgroundColor: cardColor }}
-        enableOverDrag={false}
-        style={{ zIndex: 1 }}
-        
-        backdropComponent={(props) => (
-          <BottomSheetBackdrop
-            {...props}
-            disappearsOnIndex={-1}
-            appearsOnIndex={0}
-          />
-        )}
-      >
-       <BottomSheetFlatList
-          data={terms}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => {
-                setSelectedCategory(item);
-                bottomSheetRef.current?.close();
-              }}
-              className="px-5 py-4"
-            >
-              <Text className="text-main text-lg">{item}</Text>
-            </TouchableOpacity>
-          )}
-          scrollEnabled={false}
-          className={'bg-cardColor'}
-        />
-      </BottomSheet>
-    </>
-  )
-}
 
 const _layout = () => {
   return (
-    <BottomSheetProvider>
-      <BottomSheetModalProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <InnerLayout />
-        </GestureHandlerRootView>
-      </BottomSheetModalProvider>
-    </BottomSheetProvider>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          flex: 1,
+        },
+        tabBarStyle: {
+          borderTopWidth: 0,
+          height: 70,
+          paddingBottom: 8,
+          position: 'absolute',
+        },
+        tabBarBackground: () => (
+          <View className="flex-1 bg-nav border-t border-gray-200 dark:border-transparent" />
+        ),
+      }}
+    >
+    <Tabs.Screen 
+    name="index"
+    options={{
+        title: "Courses",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+        <View className="items-center justify-center w-[180%] mt-5 h-[80%]">
+            <Ionicons name="school-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
+            <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Courses</Text>
+        </View>
+        )
+    }}
+    />
+    <Tabs.Screen 
+    name="assignments"
+    options={{
+        title: "Assignments",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+        <View className="items-center justify-center w-[240%] mt-5 h-[80%]">
+            <Ionicons name="document-text-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
+            <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Assignments</Text>
+        </View>
+        )
+    }}
+    />
+    <Tabs.Screen 
+      name="inbox"
+      options={{
+        title: "Inbox",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <View className="items-center justify-center w-[180%] mt-5 h-[80%]">
+            <Ionicons name="file-tray-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
+            <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Inbox</Text>
+          </View>
+        )
+      }}
+    />
+    <Tabs.Screen 
+    name="profile"
+    options={{
+        title: "Settings",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+        <View className="items-center justify-center w-[180%] mt-5 h-[80%]">
+            <Ionicons name="settings-outline" size={22} color={focused ? '#2A52BE' : '#A8B5DB'} />
+            <Text style={{ color: focused ? '#2A52BE' : '#A8B5DB', fontSize: 12, marginTop: 5 }}>Settings</Text>
+        </View>
+        )
+    }}
+    />
+
+    
+    </Tabs>
   )
 }
 
 export default _layout
+
+/*
+const DATA = [
+  {
+    name: 'BIOLOGY_1_HONORS',
+    teacher: 'MARISA_SPEARS',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [85, 95, 75]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 95
+    },
+  },
+  {
+    name: 'AP_PRECALCULUS',
+    teacher: 'KENZIE_SANCHEZ',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 96
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 95
+    },
+  },
+  {
+    name: 'AP_HUMAN_GEOGRAPHY',
+    teacher: 'IAN_FULLMER',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 96
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 95
+    },
+  },
+  {
+    name: 'INVENTION_&_INNOVATION_FF',
+    teacher: 'NORMAN_MORGAN',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 96
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 85
+    },
+  },
+  {
+    name: 'WATER_POLO_B_1',
+    teacher: 'DARCI_CARRUTHERS',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 96
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 95
+    },
+  },
+  {
+    name: 'ENGLISH_1_HONORS',
+    teacher: 'CATHERINE_KELLY',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 96
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 95
+    },
+  },
+  {
+    name: 'AP_COMPUTER_SCIENCE_A_Math',
+    teacher: 'ISIANA_RENDON',
+    t1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    s1: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 100
+    },
+    t3: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 98
+    },
+    t4: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 96
+    },
+    s2: {
+      categories: {
+        names: ["Daily", "Labs", "Major"],
+        grades: [90, 85, 99]
+      },
+      total: 95
+    },
+  },
+];
+*/
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,353 +1,88 @@
+import { useColorScheme, TouchableOpacity, View, Text } from "react-native";
+import { BottomSheetProvider, useBottomSheet, TermLabel } from "@/context/BottomSheetContext";
+import BottomSheet, { BottomSheetModalProvider, BottomSheetBackdrop, BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "@/utils/colorTheme";
 import { Stack } from "expo-router";
 import './globals.css';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function RootLayout() {
-  return <GestureHandlerRootView style={{ flex: 1 }}>
-  <Stack>
-    <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-    <Stack.Screen name="classes/[class]" options={{headerShown: false}}/>
-    <Stack.Screen name="assignments/[assignment]" options={{headerShown: false}}/>
-    <Stack.Screen name="inbox/[message]" options={{
-      headerShown: true,
-      title: "Message",
-      headerStyle: {
-        backgroundColor: '#2563eb'
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-        fontSize: 18,
-      },
-      headerBackTitle: 'Inbox'
-    }}/>
-  </Stack>
-  </GestureHandlerRootView>;
+
+const terms: TermLabel[] = ['Q1 Grades', 'Q2 Grades', 'SM1 Grade', 'Q3 Grades', 'Q4 Grades', 'SM2 Grades'];
+
+function InnerLayout() {
+  const { selectedCategory, setSelectedCategory, bottomSheetRef } = useBottomSheet();
+  const colorScheme = useColorScheme();
+  const cardColor = colorScheme === 'dark' ? colors.cardColor.dark : colors.cardColor.light
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+        <Stack.Screen name="classes/[class]" options={{headerShown: false}}/>
+        <Stack.Screen name="assignments/[assignment]" options={{headerShown: false}}/>
+        <Stack.Screen name="inbox/[message]" options={{
+          headerShown: true,
+          title: "Message",
+          headerStyle: {
+            backgroundColor: '#2563eb'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 18,
+          },
+          headerBackTitle: 'Inbox'
+        }}/>
+      </Stack>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={['42%']}
+        enablePanDownToClose={true}
+        backgroundStyle={{ backgroundColor: cardColor }}
+        enableOverDrag={false}
+        style={{ zIndex: 1 }}
+        
+        backdropComponent={(props) => (
+          <BottomSheetBackdrop
+            {...props}
+            disappearsOnIndex={-1}
+            appearsOnIndex={0}
+          />
+        )}
+      >
+       <BottomSheetFlatList
+          data={terms}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                setSelectedCategory(item);
+                bottomSheetRef.current?.close();
+              }}
+              className="px-5 py-4"
+            >
+              <Text className="text-main text-lg">{item}</Text>
+            </TouchableOpacity>
+          )}
+          scrollEnabled={false}
+          className={'bg-cardColor'}
+        />
+      </BottomSheet>
+    </>
+  );
 }
 
-/*
-const DATA = [
-  {
-    name: 'BIOLOGY_1_HONORS',
-    teacher: 'MARISA_SPEARS',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [85, 95, 75]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 95
-    },
-  },
-  {
-    name: 'AP_PRECALCULUS',
-    teacher: 'KENZIE_SANCHEZ',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 96
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 95
-    },
-  },
-  {
-    name: 'AP_HUMAN_GEOGRAPHY',
-    teacher: 'IAN_FULLMER',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 96
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 95
-    },
-  },
-  {
-    name: 'INVENTION_&_INNOVATION_FF',
-    teacher: 'NORMAN_MORGAN',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 96
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 85
-    },
-  },
-  {
-    name: 'WATER_POLO_B_1',
-    teacher: 'DARCI_CARRUTHERS',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 96
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 95
-    },
-  },
-  {
-    name: 'ENGLISH_1_HONORS',
-    teacher: 'CATHERINE_KELLY',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 96
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 95
-    },
-  },
-  {
-    name: 'AP_COMPUTER_SCIENCE_A_Math',
-    teacher: 'ISIANA_RENDON',
-    t1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    s1: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 100
-    },
-    t3: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 98
-    },
-    t4: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 96
-    },
-    s2: {
-      categories: {
-        names: ["Daily", "Labs", "Major"],
-        grades: [90, 85, 99]
-      },
-      total: 95
-    },
-  },
-];
-*/
+export default function RootLayout() {
+  return (
+      <BottomSheetProvider>
+        <BottomSheetModalProvider>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <InnerLayout />
+          </GestureHandlerRootView>
+        </BottomSheetModalProvider>
+      </BottomSheetProvider>
+    )
+}
+
